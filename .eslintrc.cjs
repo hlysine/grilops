3a9690cb60d64766f511715d648f38168258d46d
Henry Lin <henry.ys.lin@gmail.com>
2024-04-09 11:14:10 +0800

    Set up linters and formatters

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      './tsconfig.json',
+      './tsconfig.build.json',
+      './tsconfig.node.json',
+    ],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'standard',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unnecessary-condition': 'off',
+    '@typescript-eslint/class-literal-property-style': 'off',
+    '@typescript-eslint/ban-types': ['error', { types: { Symbol: false } }],
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/unified-signatures': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+  },
+};
--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,6 @@ import { init } from 'z3-solver';
 export async function z3Solve() {
   const { Context } = await init();
   const { Solver, Int, And } = Context('main');
-
   const x = Int.const('x');
 
   const solver = new Solver();

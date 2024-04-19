62e78afa747c674a6dfe5d0d1d4d8e140c8ae08f
Henry Lin <henry.ys.lin@gmail.com>
2024-04-19 23:15:52 +0800

    Complete geometry

--- lib/geometry.ts
+++ lib/geometry.ts
@@ -42,7 +42,7 @@ export class Vector {
   }
 }
 
-export type DirectionString = 'N' | 'S' | 'E' | 'W';
+export type DirectionString = 'N' | 'S' | 'E' | 'W' | 'NE' | 'NW' | 'SE' | 'SW';
 
 /**
  * A named direction vector that offsets by one space in the grid.
@@ -96,6 +96,20 @@ export class Point {
   public toString(): PointString {
     return `P(${this.y},${this.x})`;
   }
+
+  public static comparator(a: Point, b: Point): number {
+    if (a.y < b.y) {
+      return -1;
+    } else if (a.y > b.y) {
+      return 1;
+    } else if (a.x < b.x) {
+      return -1;
+    } else if (a.x > b.x) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
 
 export type HookFunction = (point: Point) => string | undefined;
@@ -127,7 +141,7 @@ export class Neighbor<Name extends string> {
 /**
  * A base class for defining the structure of a grid.
  */
-export class Lattice {
+export abstract class Lattice {
   private _vectorDirection: Map<VectorString, Direction>;
 
   public constructor() {
@@ -140,18 +154,15 @@ export class Lattice {
   /**
    * The points in the lattice, sorted.
    */
-  public get points(): Point[] {
-    throw new Error('Not implemented');
-  }
+  public abstract get points(): Point[];
 
   /**
    * Returns the index of a point in the lattice's ordered list.
    * @param point The `Point` to get the index of.
-   * @returns The index of the point in the ordered list, or undefined if the point is not in the list.
+   * @returns The index of the point in the ordered list, or None if the point
+   * is not in the list.
    */
-  public pointToIndex(point: Point): number | undefined {
-    throw new Error('Not implemented');
-  }
+  public abstract pointToIndex(point: Point): number | undefined;
 
   /**
    * A list of edge-sharing directions.
@@ -160,9 +171,7 @@ export class Lattice {
    * known as orthogonal adjacency) is the relationship between grid cells
    * that share an edge.
    */
-  public edgeSharingDirections(): Direction[] {
-    throw new Error('Not implemented');
-  }
+  public abstract edgeSharingDirections(): Direction[];
 
   /**
    * A list of vertex-sharing directions.
@@ -171,9 +180,7 @@ export class Lattice {
    * direction. Vertex sharing (also known as touching adjacency) is the
    * relationship between grid cells that share a vertex.
    */
-  public vertexSharingDirections(): Direction[] {
-    throw new Error('Not implemented');
-  }
+  public abstract vertexSharingDirections(): Direction[];
 
   /**
    * Given a direction, return the opposite direction.
@@ -266,9 +273,7 @@ export class Lattice {
    * @returns A label representing the direction.
    * @throws An error if there's no character defined for the direction.
    */
-  public labelForDirection(direction: Direction): string {
-    throw new Error('Not implemented');
-  }
+  public abstract labelForDirection(direction: Direction): string;
 
   /**
    * Returns the label for a pair of edge-sharing directions.
@@ -277,9 +282,10 @@ export class Lattice {
    * @returns A label representing both directions.
    * @throws An error if there's no character defined for the direction pair.
    */
-  public labelForDirectionPair(dir1: Direction, dir2: Direction): string {
-    throw new Error('Not implemented');
-  }
+  public abstract labelForDirectionPair(
+    dir1: Direction,
+    dir2: Direction
+  ): string;
 
   /**
    * Returns a list of `Vector` transformations.
@@ -293,12 +299,10 @@ export class Lattice {
    * @param allowReflections Whether reflection is an allowed transformation.
    * @returns A list of `Vector` transformation functions.
    */
-  public transformationFunctions(
+  public abstract transformationFunctions(
     allowRotations: boolean,
     allowReflections: boolean
-  ): ((vector: Vector) => Vector)[] {
-    throw new Error('Not implemented');
-  }
+  ): ((vector: Vector) => Vector)[];
 
   /**
    * Returns directions for use in a loop inside-outside check.
@@ -315,9 +319,7 @@ export class Lattice {
    * look, and the second component of which indicates what types of crossings
    * to count.
    */
-  public getInsideOutsideCheckDirections(): [Direction, Direction[]] {
-    throw new Error('Not implemented');
-  }
+  public abstract getInsideOutsideCheckDirections(): [Direction, Direction[]];
 
   /**
    * Prints something for each of the given points.
@@ -383,4 +385,387 @@ export class Lattice {
     }
     return ret;
   }
+}
+
+export class RectangularLattice extends Lattice {
+  private _points: Point[];
+  private _pointIndices: Map<PointString, number>;
+
+  /**
+   * @param points A set of points corresponding to a rectangular lattice.
+   * Note that these points need not fill a complete rectangle.
+   */
+  public constructor(points: Point[]) {
+    super();
+    this._points = points.sort(Point.comparator);
+    this._pointIndices = new Map();
+    this._points.forEach((p, i) => {
+      this._pointIndices.set(p.toString(), i);
+    });
+  }
+
+  public get points(): Point[] {
+    return this._points;
+  }
+
+  public pointToIndex(point: Point): number | undefined {
+    return this._pointIndices.get(point.toString());
+  }
+
+  public edgeSharingDirections(): Direction[] {
+    return [
+      new Direction('N', new Vector(-1, 0)),
+      new Direction('S', new Vector(1, 0)),
+      new Direction('E', new Vector(0, 1)),
+      new Direction('W', new Vector(0, -1)),
+    ];
+  }
+
+  public vertexSharingDirections(): Direction[] {
+    return [
+      ...this.edgeSharingDirections(),
+      new Direction('NE', new Vector(-1, 1)),
+      new Direction('NW', new Vector(-1, -1)),
+      new Direction('SE', new Vector(1, 1)),
+      new Direction('SW', new Vector(1, -1)),
+    ];
+  }
+
+  public labelForDirection(direction: Direction): string {
+    if (direction.name === 'N') {
+      return '\u2575';
+    }
+    if (direction.name === 'E') {
+      return '\u2576';
+    }
+    if (direction.name === 'S') {
+      return '\u2577';
+    }
+    if (direction.name === 'W') {
+      return '\u2574';
+    }
+    throw new Error('No single-character symbol for direction');
+  }
+
+  public labelForDirectionPair(dir1: Direction, dir2: Direction): string {
+    if (dir1.name === 'N' && dir2.name === 'S') {
+      return '\u2502';
+    }
+    if (dir1.name === 'E' && dir2.name === 'W') {
+      return '\u2500';
+    }
+    if (dir1.name === 'N' && dir2.name === 'E') {
+      return '\u2514';
+    }
+    if (dir1.name === 'S' && dir2.name === 'E') {
+      return '\u250c';
+    }
+    if (dir1.name === 'S' && dir2.name === 'W') {
+      return '\u2510';
+    }
+    if (dir1.name === 'N' && dir2.name === 'W') {
+      return '\u2518';
+    }
+    throw new Error('No single-character symbol for direction pair');
+  }
+
+  public transformationFunctions(
+    allowRotations: boolean,
+    allowReflections: boolean
+  ): ((vector: Vector) => Vector)[] {
+    if (allowRotations) {
+      if (allowReflections) {
+        return [
+          v => v,
+          v => new Vector(v.dy, -v.dx),
+          v => new Vector(-v.dy, v.dx),
+          v => new Vector(-v.dy, -v.dx),
+          v => new Vector(v.dx, v.dy),
+          v => new Vector(v.dx, -v.dy),
+          v => new Vector(-v.dx, v.dy),
+          v => new Vector(-v.dx, -v.dy),
+        ];
+      } else {
+        return [
+          v => v,
+          v => new Vector(v.dx, -v.dy),
+          v => new Vector(-v.dy, -v.dx),
+          v => new Vector(-v.dx, v.dy),
+        ];
+      }
+    } else {
+      if (allowReflections) {
+        return [
+          v => v,
+          v => new Vector(v.dy, -v.dx),
+          v => new Vector(-v.dy, v.dx),
+        ];
+      } else {
+        return [v => v];
+      }
+    }
+  }
+
+  public getInsideOutsideCheckDirections(): [Direction, Direction[]] {
+    const ds = this.edgeSharingDirections();
+    return [ds.find(x => x.name === 'N')!, [ds.find(x => x.name === 'W')!]];
+  }
+}
+
+export abstract class HexagonalLattice extends Lattice {
+  private _points: Point[];
+  private _pointIndices: Map<PointString, number>;
+  private static _DIRECTION_LABELS = {
+    N: '\u2595 \n  ',
+    NE: ' \u2571\n  ',
+    E: ' \u2581\n  ',
+    SE: '  \n \u2572',
+    S: '  \n\u2595 ',
+    SW: '  \n\u2571 ',
+    W: '\u2581 \n  ',
+    NW: '\u2572 \n  ',
+  };
+
+  /**
+   * A set of points forming a hexagonal lattice.
+   *
+   * This abstract class implements functions identical between
+   * FlatToppedHexagonalLattice and PointyToppedHexagonalLattice.
+   *
+   * We use the doubled coordinates scheme described at
+   * https://www.redblobgames.com/grids/hexagons/. That is, y describes
+   * the row and x describes the column, so x + y is always even.
+   */
+  public constructor(points: Point[]) {
+    super();
+    for (const p of points) {
+      if ((p.y + p.x) % 2 === 1) {
+        throw new Error('Hexagonal coordinates must have an even sum.');
+      }
+    }
+    this._points = points.sort(Point.comparator);
+    this._pointIndices = new Map();
+    this._points.forEach((p, i) => {
+      this._pointIndices.set(p.toString(), i);
+    });
+  }
+
+  public get points(): Point[] {
+    return this._points;
+  }
+
+  public pointToIndex(point: Point): number | undefined {
+    return this._pointIndices.get(point.toString());
+  }
+
+  public vertexSharingDirections(): Direction[] {
+    return this.edgeSharingDirections();
+  }
+
+  public labelForDirection(direction: Direction): string {
+    const label = HexagonalLattice._DIRECTION_LABELS[direction.name];
+    if (label !== undefined) {
+      return label;
+    }
+    throw new Error('No single-character symbol for direction');
+  }
+
+  public labelForDirectionPair(dir1: Direction, dir2: Direction): string {
+    let ds = [dir1.name, dir2.name];
+    function charForPos(dirs: string[], chars: string[]) {
+      for (const [d, c] of zip(dirs, chars)) {
+        if (ds.includes(d as DirectionString)) {
+          ds = ds.filter(x => x !== d);
+          return c.toString();
+        }
+      }
+      return ' ';
+    }
+    const ul = charForPos(['NW', 'N', 'W'], ['\u2572', '\u2595', '\u2581']);
+    const ur = charForPos(['NE', 'N', 'E'], ['\u2571', '\u258f', '\u2581']);
+    const ll = charForPos(['SW', 'S', 'W'], ['\u2571', '\u2595', '\u2594']);
+    const lr = charForPos(['SE', 'S', 'E'], ['\u2572', '\u258f', '\u2594']);
+    return `${ul}${ur}\n${ll}${lr}`;
+  }
+}
+
+/**
+ * A set of points forming a flat-topped hexagonal lattice.
+ *
+ * All points must lie on a hexagonal lattice in which each hexagon has
+ * a flat top. We use the doubled coordinates scheme described at
+ * https://www.redblobgames.com/grids/hexagons/. That is, y describes
+ * the row and x describes the column, so hexagons that are vertically
+ * adjacent have their y coordinates differ by 2.
+ */
+export class FlatToppedHexagonalLattice extends HexagonalLattice {
+  public edgeSharingDirections(): Direction[] {
+    return [
+      new Direction('N', new Vector(-2, 0)),
+      new Direction('S', new Vector(2, 0)),
+      new Direction('NE', new Vector(-1, 1)),
+      new Direction('NW', new Vector(-1, -1)),
+      new Direction('SE', new Vector(1, 1)),
+      new Direction('SW', new Vector(1, -1)),
+    ];
+  }
+
+  public transformationFunctions(
+    allowRotations: boolean,
+    allowReflections: boolean
+  ): ((vector: Vector) => Vector)[] {
+    if (allowRotations) {
+      if (allowReflections) {
+        return [
+          v => v,
+          v => new Vector((v.dy + 3 * v.dx) / 2, (-v.dy + v.dx) / 2),
+          v => new Vector((-v.dy + 3 * v.dx) / 2, (-v.dy - v.dx) / 2),
+          v => new Vector(-v.dy, -v.dx),
+          v => new Vector((-v.dy - 3 * v.dx) / 2, (v.dy - v.dx) / 2),
+          v => new Vector((v.dy - 3 * v.dx) / 2, (v.dy + v.dx) / 2),
+          v => new Vector(-v.dy, v.dx),
+          v => new Vector((-v.dy - 3 * v.dx) / 2, (-v.dy + v.dx) / 2),
+          v => new Vector((v.dy - 3 * v.dx) / 2, (-v.dy - v.dx) / 2),
+          v => new Vector(v.dy, -v.dx),
+          v => new Vector((v.dy + 3 * v.dx) / 2, (v.dy - v.dx) / 2),
+          v => new Vector((-v.dy + 3 * v.dx) / 2, (v.dy + v.dx) / 2),
+        ];
+      } else {
+        return [
+          v => v,
+          v => new Vector((v.dy + 3 * v.dx) / 2, (-v.dy + v.dx) / 2),
+          v => new Vector((-v.dy + 3 * v.dx) / 2, (-v.dy - v.dx) / 2),
+          v => new Vector(-v.dy, -v.dx),
+          v => new Vector((-v.dy - 3 * v.dx) / 2, (v.dy - v.dx) / 2),
+          v => new Vector((v.dy - 3 * v.dx) / 2, (v.dy + v.dx) / 2),
+        ];
+      }
+    } else {
+      if (allowReflections) {
+        return [
+          v => v,
+          v => new Vector(-v.dy, v.dx),
+          v => new Vector((-v.dy - 3 * v.dx) / 2, (-v.dy + v.dx) / 2),
+          v => new Vector((v.dy - 3 * v.dx) / 2, (-v.dy - v.dx) / 2),
+          v => new Vector(v.dy, -v.dx),
+          v => new Vector((v.dy + 3 * v.dx) / 2, (v.dy - v.dx) / 2),
+          v => new Vector((-v.dy + 3 * v.dx) / 2, (v.dy + v.dx) / 2),
+        ];
+      } else {
+        return [v => v];
+      }
+    }
+  }
+
+  public getInsideOutsideCheckDirections(): [Direction, Direction[]] {
+    const ds = this.edgeSharingDirections();
+    return [
+      ds.find(x => x.name === 'N')!,
+      [ds.find(x => x.name === 'NW')!, ds.find(x => x.name === 'SW')!],
+    ];
+  }
+}
+
+/**
+ * A set of points forming a pointy-topped hexagonal lattice.
+ *
+ * All points must lie on a hexagonal lattice in which each hexagon has
+ * a pointy top. We use the doubled coordinates scheme described at
+ * https://www.redblobgames.com/grids/hexagons/. That is, y describes
+ * the row and x describes the column, so hexagons that are horizontally
+ * adjacent have their x coordinates differ by 2.
+ */
+export class PointyToppedHexagonalLattice extends HexagonalLattice {
+  public edgeSharingDirections(): Direction[] {
+    return [
+      new Direction('E', new Vector(0, 2)),
+      new Direction('W', new Vector(0, -2)),
+      new Direction('NE', new Vector(-1, 1)),
+      new Direction('NW', new Vector(-1, -1)),
+      new Direction('SE', new Vector(1, 1)),
+      new Direction('SW', new Vector(1, -1)),
+    ];
+  }
+
+  public transformationFunctions(
+    allowRotations: boolean,
+    allowReflections: boolean
+  ): ((vector: Vector) => Vector)[] {
+    if (allowRotations) {
+      if (allowReflections) {
+        return [
+          v => v,
+          v => new Vector((v.dy + v.dx) / 2, (-3 * v.dy + v.dx) / 2),
+          v => new Vector((-v.dy + v.dx) / 2, (-3 * v.dy - v.dx) / 2),
+          v => new Vector(-v.dy, -v.dx),
+          v => new Vector((-v.dy - v.dx) / 2, (3 * v.dy - v.dx) / 2),
+          v => new Vector((v.dy - v.dx) / 2, (3 * v.dy + v.dx) / 2),
+          v => new Vector(-v.dy, v.dx),
+          v => new Vector((-v.dy - v.dx) / 2, (-3 * v.dy + v.dx) / 2),
+          v => new Vector((v.dy - v.dx) / 2, (-3 * v.dy - v.dx) / 2),
+          v => new Vector(v.dy, -v.dx),
+          v => new Vector((v.dy + v.dx) / 2, (3 * v.dy - v.dx) / 2),
+          v => new Vector((-v.dy + v.dx) / 2, (3 * v.dy + v.dx) / 2),
+        ];
+      } else {
+        return [
+          v => v,
+          v => new Vector((v.dy + v.dx) / 2, (-3 * v.dy + v.dx) / 2),
+          v => new Vector((-v.dy + v.dx) / 2, (-3 * v.dy - v.dx) / 2),
+          v => new Vector(-v.dy, -v.dx),
+          v => new Vector((-v.dy - v.dx) / 2, (3 * v.dy - v.dx) / 2),
+          v => new Vector((v.dy - v.dx) / 2, (3 * v.dy + v.dx) / 2),
+        ];
+      }
+    } else {
+      if (allowReflections) {
+        return [
+          v => new Vector(-v.dy, v.dx),
+          v => new Vector((-v.dy - v.dx) / 2, (-3 * v.dy + v.dx) / 2),
+          v => new Vector((v.dy - v.dx) / 2, (-3 * v.dy - v.dx) / 2),
+          v => new Vector(v.dy, -v.dx),
+          v => new Vector((v.dy + v.dx) / 2, (3 * v.dy - v.dx) / 2),
+          v => new Vector((-v.dy + v.dx) / 2, (3 * v.dy + v.dx) / 2),
+        ];
+      } else {
+        return [v => v];
+      }
+    }
+  }
+
+  public getInsideOutsideCheckDirections(): [Direction, Direction[]] {
+    const ds = this.edgeSharingDirections();
+    return [
+      ds.find(x => x.name === 'E')!,
+      [ds.find(x => x.name === 'NW')!, ds.find(x => x.name === 'NE')!],
+    ];
+  }
+}
+
+/**
+ * Returns a lattice of all points in a rectangle of the given dimensions.
+ * @param height Height of the lattice.
+ * @param width Width of the lattice.
+ * @returns The lattice.
+ */
+export function getRectangleLattice(
+  height: number,
+  width: number
+): RectangularLattice {
+  const points: Point[] = [];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      points.push(new Point(y, x));
+    }
+  }
+  return new RectangularLattice(points);
+}
+
+/**
+ * Returns a lattice of all points in a square of the given height.
+ * @param height Height of the lattice.
+ * @returns The lattice.
+ */
+export function getSquareLattice(height: number): RectangularLattice {
+  return getRectangleLattice(height, height);
 }

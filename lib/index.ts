698eda2b4c750949affc3c0ac23064352eb5b341
Henry Lin <henry.ys.lin@gmail.com>
2024-04-20 16:44:27 +0800

    Export one function to contextualize all modules

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,23 @@
+import fastZ3 from './utils/fastZ3';
+import { GrilopsContext } from './utils/utils';
+import * as geometry from './geometry';
+import grids from './grids';
+import quadTree from './utils/quadTree';
+import * as symbols from './symbols';
+
 export * from './utils/fastZ3';
 export * from './utils/utils';
 export * from './geometry';
 export * from './grids';
 export * from './utils/quadTree';
 export * from './symbols';
+
+export function grilops<Name extends string>(context: GrilopsContext<Name>) {
+  return {
+    ...fastZ3(context),
+    ...geometry,
+    ...grids(context),
+    ...quadTree(context),
+    ...symbols,
+  };
+}
